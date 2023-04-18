237aad1bc2f4079c08b03829a3e38730d25c03cd
Jasa Paavolainen <jasa.paavolainen@hotmail.com>
2023-04-18 13:12:52 +0300

    Week 4
    Changes and additions made on Week 4

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './components/Login';
+import Register from './components/Register';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+
+class AppRouter extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <nav className="main-nav">
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<AppRouter />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
